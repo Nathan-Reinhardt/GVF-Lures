79b5a609aa53797edd18327f6bc1b01e5acb0afa
Ticon Reinhardt <nreinhardt1234@gmail.com>
2023-08-31 22:15:18 -0700

    added phone number and unicode telephone icon on splash page

--- backend_controller/frontend/src/components/SplashPage.js
+++ backend_controller/frontend/src/components/SplashPage.js
@@ -13,6 +13,13 @@ export default class SplashPage extends Component {
                 <Navbar />
                 <div className="gvf-logo-container">
                     <Link className="site-title" to="/">GVF Lures</Link>
+                    <div className="phone-num-cont">
+                        <h3 className="red-phone-icon">&#128222;</h3>
+                        <h3 className="contact-number">(916) 531-6011</h3>
+                    </div>
+                </div>
+                <div className="">
+
                 </div>
             </div>
         );
