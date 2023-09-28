6c8378b83b762ebe80783c2fc91f45fc3a339495
Ticon Reinhardt <nreinhardt1234@gmail.com>
2023-09-27 19:11:04 -0700

    css adjustments and added more images to work on slideshow

--- backend_controller/frontend/src/components/InfoNavPanel/HeaderDetail.js
+++ backend_controller/frontend/src/components/InfoNavPanel/HeaderDetail.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';   // NOTE: IMAGES Import is never used, but it is actually
 import { Link } from 'react-router-dom';    // being used by webpack to make it readable to the browser
-import IMAGES from '../images/_images';
+import IMAGES from '../_images';
 
 export default class HeaderDetail extends Component {
     constructor(props) {
@@ -9,13 +9,11 @@ export default class HeaderDetail extends Component {
 
     render() {
         return (
-            <div className="gvf-logo-container">
-                <div className="gvf-lures-logo-image">
-                    <Link className="site-logo-link" to="/"></Link>
-                </div>
-                <div className="phone-num-cont">
-                    <h3 className="red-phone-icon">&#128222;</h3>
-                    <h3 className="contact-number">(916) 531-6011</h3>
+            <div className="header-detail-container">
+                <div className="gvf-logo-container">
+                    <div className="gvf-lures-logo-image">
+                        <Link className="site-logo-link" to="/"></Link>
+                    </div>
                 </div>
             </div>
         );
---
+++ backend_controller/frontend/src/components/_images.js
@@ -0,0 +1,45 @@
+// This file is used for webpack to recognize images and 
+// make the browser also recognize images in the bundle file.
+
+const IMAGES = {
+    // Important Images
+    GFVLuresLogo: require("./images/GVFLuresLogo.jpg").default,
+    Rich1: require("./images/Rich1.jpg").default,
+    Rich2: require("./images/Rich2.jpg").default,
+    RichnMe: require("./images/RichnMe.jpg").default,
+    TigerLures: require("./images/TigerLures.jpg").default,
+
+    // Fishing and People
+    FishingP1: require("./images/FishingP1.jpg").default,
+    FishingP2: require("./images/FishingP2.jpg").default,
+    FishingP3: require("./images/FishingP3.jpg").default,
+    FishingP4: require("./images/FishingP4.jpg").default,
+    FishingP5: require("./images/FishingP5.jpg").default,
+    FishingP6: require("./images/FishingP6.jpg").default,
+    FishingP7: require("./images/FishingP7.jpg").default,
+    FishingP8: require("./images/FishingP8.jpg").default,
+    FishingP9: require("./images/FishingP9.jpg").default,
+    FishingP10: require("./images/FishingP10.jpg").default,
+    FishingP11: require("./images/FishingP11.jpg").default,
+
+    // Fish and Lures
+    Fish_Lures1: require("./images/Fish_Lures1.jpg").default,
+    Fish_Lures2: require("./images/Fish_Lures2.jpg").default,
+    Fish_Lures3: require("./images/Fish_Lures3.jpg").default,
+    Fish_Lures4: require("./images/Fish_Lures4.jpg").default,
+    Fish_Lures5: require("./images/Fish_Lures5.jpg").default,
+    Fish_Lures6: require("./images/Fish_Lures6.jpg").default,
+    Fish_Lures7: require("./images/Fish_Lures7.jpg").default,
+    Fish_Lures8: require("./images/Fish_Lures8.jpg").default,
+    Fish_Lures9: require("./images/Fish_Lures9.jpg").default,
+    Fish_Lures10: require("./images/Fish_Lures10.jpg").default,
+
+    // Slide Images
+    Slide1: require("./images/Slide1.jpg").default,
+    Slide2: require("./images/Slide2.jpg").default,
+
+    // Goofy
+    GoofyImageOfMe: require("./images/GoofyImageOfMe.jpg").default
+}
+
+export default IMAGES;
