2d5b83e2da8dca6ca98dc1b78a12d4a60ed2eb88
Ticon Reinhardt <nreinhardt1234@gmail.com>
2023-11-26 22:24:23 -0800

    added responsiveness to middle navbar, menu dropdown and menu creation

--- backend_controller/frontend/src/components/InfoNavPanel/MiddleNavbar.js
+++ backend_controller/frontend/src/components/InfoNavPanel/MiddleNavbar.js
@@ -1,41 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { EssentialMedia } from '../../utils/_essentialmedia';
 
-export default class MiddleNavbar extends Component {
-    constructor(props) {
-        super(props);
+const MiddleNavbar = () => {
+
+    const token = localStorage.getItem("authTokens");
+    const [isActive, setIsActive] = useState(false);
+
+    // when the user clicks the menu button it dropsdown or closes the menu dropdown
+    const menuDropdown = () => {
+        if (isActive) {
+            setIsActive(false);
+        }
+        else {
+            setIsActive(true);
+        }
     }
 
-    render() {
-        const token = localStorage.getItem("authTokens");
-        
-        return (
-            <div>
-                <div className="middle-nav-cont">
-                    <ul className="middle-nav-list">
-                        <li className="home-middle-li">
-                            <Link className="to-page" to="/">Home</Link>
+    return (
+        <div>
+            <div className="middle-nav-cont">
+                <ul className="middle-nav-list">
+                    <li className="home-middle-li">
+                        <Link className="to-page" to="/">Home</Link>
+                    </li>
+                    <li className="contact-middle-li">
+                        <Link className="to-page" to="/contact">Contact Us</Link>
+                    </li>
+                    <li className="lakes-middle-li">
+                        <Link className="to-page" to="/lakes">Lakes & Reservoirs</Link>
+                    </li>
+                    <li className="gallery-middle-li">
+                        <Link className="to-page" to="/gallery">Gallery</Link>
+                    </li>
+                    {token === null && <></>}
+                    {token !== null &&
+                    <>
+                        <li className="profile-middle-li">
+                            <Link className="to-page" to="/profile">Profile</Link>
                         </li>
-                        <li className="contact-middle-li">
-                            <Link className="to-page" to="/contact">Contact Us</Link>
-                        </li>
-                        <li className="lakes-middle-li">
-                            <Link className="to-page" to="/lakes">Lakes & Reservoirs</Link>
-                        </li>
-                        <li className="gallery-middle-li">
-                            <Link className="to-page" to="/gallery">Gallery</Link>
-                        </li>
-                        {token === null && <></>}
-                        {token !== null &&
-                        <>
-                            <li className="profile-middle-li">
-                                <Link className="to-page" to="/profile">Profile</Link>
-                            </li>
-                        </>
-                        }
-                    </ul>
+                    </>
+                    }
+                </ul>
+            </div>
+            <div className="menu-nav-cont">
+                <div id="menu" className="menu-nav-closed" onClick={menuDropdown}>
+                    <li className="menu-text">Menu</li>
                 </div>
             </div>
-        );
-    };
+            <ul className="menu-nav-list" style={{ display: isActive ? "block" : "none"}}>
+                <li className="home-menu-li">
+                    <Link className="to-page" to="/">Home</Link>
+                </li>
+                <li className="contact-menu-li">
+                    <Link className="to-page" to="/contact">Contact Us</Link>
+                </li>
+                <li className="lakes-menu-li">
+                    <Link className="to-page" to="/lakes">Lakes & Reservoirs</Link>
+                </li>
+                <li className="gallery-menu-li">
+                    <Link className="to-page" to="/gallery">Gallery</Link>
+                </li>
+                {token === null && <></>}
+                {token !== null &&
+                <>
+                    <li className="profile-menu-li">
+                        <Link className="to-page" to="/profile">Profile</Link>
+                    </li>
+                </>
+                }
+            </ul>
+        </div>
+    );
 };
+
+export default MiddleNavbar;
