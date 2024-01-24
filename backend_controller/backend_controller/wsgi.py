866a023b664ca0eea933f31c8200e1b876057d0b
Ticon Reinhardt <nreinhardt1234@gmail.com>
2024-01-23 20:43:19 -0800

    reverting changes

--- backend_controller/backend_controller/wsgi.py
+++ backend_controller/backend_controller/wsgi.py
@@ -8,16 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
 from django.core.wsgi import get_wsgi_application
-from backend_controller import urls
-
-# Get the directory containing the wsgi.py file (two levels up)
-project_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Add the project's root directory to the Python path
-sys.path.insert(0, project_directory)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_controller.backend_controller.settings')
 
