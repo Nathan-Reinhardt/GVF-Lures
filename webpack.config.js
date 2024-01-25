32c2503e3d2e94fdb6578a084428a9c791b26186
Ticon Reinhardt <nreinhardt1234@gmail.com>
2024-01-25 01:48:59 -0800

    reseting to previous static file setup

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (_, argv) => {
     entry: "./index.js",
     output: {
       filename: "bundle.js",
-      path: path.resolve(__dirname, "staticfiles"),
+      path: path.resolve(__dirname, "frontend/static"),
     },
     module: {
       rules: [
