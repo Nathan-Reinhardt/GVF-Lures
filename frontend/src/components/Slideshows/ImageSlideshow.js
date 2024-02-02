10e9fa053f1f50e7bd4bd022bc118218429b0541
Ticon Reinhardt <nreinhardt1234@gmail.com>
2024-02-02 15:27:14 -0800

    made adjustments to TextSlideshowOverlays to have a delay on when the text overlay appears over the slideshow

--- frontend/src/components/Slideshows/ImageSlideshow.js
+++ frontend/src/components/Slideshows/ImageSlideshow.js
@@ -4,18 +4,16 @@ import TextSlideshowOverlays from './TextSlideshowOverlays';
 
 const ImageSlideshow = ({ images }) => {
     const {user} = useContext(AuthContext);     // grab the user and token
-    let username = "";
-    
-    // used to make sure React doesn't break when user doesn't exist
-    if (user === null) {
-        username = "none";
-    }
-    else {
-        username = user.username;
-    }
 
     // to keep track of current index
     const [currentIndex, setCurrentIndex] = useState(0);
+
+    // props to send into TextSlideshowOverlays
+    // used to make sure React doesn't break when user doesn't exist
+    const username = user ? user.username : "none";
+
+    // list of strings to use in the text overlays for each slide
+    const textOverlays = user ? ["Welcome " + username + " to GVF LURES!", "No Species Is Safe"] : ["Welcome to GVF LURES!", "No Species Is Safe"];
 
     // change the number for how many miliseconds between images
     const intervalNum = 15000;
@@ -38,7 +36,7 @@ const ImageSlideshow = ({ images }) => {
                     className={`slide ${index === currentIndex ? 'active' : ''}`}
                 />
             ))}
-            <TextSlideshowOverlays username={username} />
+            <TextSlideshowOverlays textOverlays={textOverlays} intervalNum={intervalNum} />
         </div>
     );
 };
--- frontend/src/components/Slideshows/TextSlideshowOverlays.js
+++ frontend/src/components/Slideshows/TextSlideshowOverlays.js
@@ -1,31 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
-const TextSlideshowOverlays = (props) => {
-    const token = localStorage.getItem("authTokens");   // if token is null, the user doesn't exist
-
+const TextSlideshowOverlays = ({ textOverlays, intervalNum }) => {
     // to keep track of current index
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         // to switch to the next image
         const nextText = () => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % 3);
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % textOverlays.length);
         };
 
         // change the number for how many miliseconds between images
-        const intervalId = setInterval(nextText, 15000);
+        const intervalId = setInterval(nextText, intervalNum);
 
         return () => clearInterval(intervalId);
-    });
+    }, [textOverlays, intervalNum]);
 
     // when you continue make sure to maybe pass props into this component for a text list
     // also props being sent into the component might not be the best solution so keep testing
 
     return (
         <div>
-            <div className={`overlay-text ${0 === currentIndex ? 'active' : ''}`}>
-                Welcome TO GVF LURES!
-            </div>
+            {textOverlays.map((text, index) => (
+                <div key={index} className={`overlay-text ${index === currentIndex ? 'active' : ''}`}>
+                    {text}
+                </div>
+            ))}
         </div>
     );
 };
